cf7e4d9fdab6e3187df710a80161458f858b7521
Matt Zeunert <matt@mostlystatic.com>
2017-01-09 09:19:45 +0000

    Add tests.

--- ohd.js
+++ ohd.js
@@ -1,1 +1,170 @@
-alert("sth")
+function HistoryEntry(propertyName){
+    this.lastAssignment = null;
+    this.fullHistory = [];
+    this.propertyName = propertyName
+}
+HistoryEntry.prototype.clone = function(){
+    return {
+        fullHistory: this.fullHistory.slice().map(assignment => {
+            return {
+                value: assignment.value,
+                stack: assignment.stack.slice()
+            }
+        })
+    }
+}
+HistoryEntry.prototype.prettyPrint = function(){
+    var fullHistory = this.clone().fullHistory;
+    var self = this;
+
+    var framesLeftToResolve = 0;
+
+    debugger
+    fullHistory.forEach(function(assignment, assignmentIndex){
+        assignment.stack.forEach(function(frameString, frameIndex){
+            framesLeftToResolve++;
+
+            codePreprocessor.resolveFrame(frameString, function(err, resolvedFrame){
+                framesLeftToResolve--;
+
+                fullHistory[assignmentIndex].stack[frameIndex] = resolvedFrame
+
+                if (framesLeftToResolve===0){
+                    console.log("done")
+                    self._log(fullHistory)
+                }
+            })
+        })
+    })
+}
+HistoryEntry.prototype.prettyPrintSortOf = function(){
+    var hist = this;
+
+    this._log(hist.fullHistory)
+}
+HistoryEntry.prototype._log = function(fullHistory){
+    console.group("%c Most recent '" + this.propertyName + "' assignments:", "color: red; text-transform: uppercase; font-weight: bold; font-size: 10px")
+    fullHistory.forEach((assignment, i) => {
+        var frame = assignment.stack[0]
+        if (typeof frame === "string"){
+            console.log("[" + i + "] Set to ", assignment.value, frame)
+        } else {
+            console.log("[" + i + "] Set to ", assignment.value)
+            logFrameObject(frame, false)
+        }
+
+        console.groupCollapsed("%c MORE", "font-weight: bold; font-size: 7px;color: #777")
+        assignment.stack.forEach(function(frame){
+            if (typeof frame === "string") {
+                console.log(frame)
+            } else if (typeof frame === "object") {
+                logFrameObject(frame, true)
+            }
+        })
+        console.groupEnd()
+    })
+    console.groupEnd()
+
+    function logFrameObject(frame, isDetailed){
+        var path = frame.fileName.replace(".dontprocess", "")
+        var parts = path.split("/")
+        var fileName = parts[parts.length - 1]
+        console.log("Original location:", fileName + ":" + frame.lineNumber + ":" + frame.columnNumber)
+        if (isDetailed){
+            var previousLine = frame.prevLines[frame.prevLines.length - 1]
+            var nextLine = frame.nextLines[0]
+            if (previousLine){
+                console.log("###", previousLine)
+            }
+            console.log(">>>", frame.line)
+            if (nextLine) {
+                console.log("###", nextLine)
+            }
+        } else {
+            console.log(frame.line)
+        }
+    }
+}
+Object.defineProperty(HistoryEntry.prototype, "clickDotsToPrettyPrintSortOf", {
+    get: function(){
+        this.prettyPrintSortOf()
+        return "Printed to console"
+    }
+})
+
+window.__ohdAssign = function(object, propertyName, value){
+    var propertyNameString = propertyName.toString()
+    var storagePropName = propertyNameString + "__history__";
+
+    var propertyNameType = typeof propertyName;
+    // Either Symbol() or Object(Symbol())
+    var propertyNameIsSymbol = propertyNameType === "symbol" || propertyNameString === "Symbol()"
+    if (!propertyNameIsSymbol) {
+        if (propertyName === null
+            || propertyName === undefined
+            || (propertyNameType !== "string")) {
+            propertyName = propertyNameString;
+        }
+    }
+
+    if (object[storagePropName] === undefined){
+        Object.defineProperty(object, storagePropName, {
+            value: new HistoryEntry(propertyNameString),
+            enumerable: false,
+            writable: true
+        })
+    }
+
+    Error.stackTraceLimit = 100
+    var stack = Error().stack.split("\n")
+    stack = stack.filter(frameString => {
+        if (frameString === "Error") {
+            return false;
+        }
+        if (frameString.indexOf("__ohd") !== -1) {
+            return false;
+        }
+        if (frameString.indexOf("<anonymous>:") !== -1){
+            return false;
+        }
+        return true;
+    })
+
+    object[storagePropName].lastAssignment = stack[0]
+    object[storagePropName].fullHistory.unshift({
+        value: value,
+        stack
+    })
+
+    if (object[storagePropName].fullHistory.length > 20) {
+        object[storagePropName].fullHistory = object[storagePropName].fullHistory.slice(0, 20)
+    }
+
+    return object[propertyName] = value
+}
+
+window.__ohdMakeObject = function(properties){
+    var obj = {}
+    var methodProperties = {};
+    for (var i=0; i< properties.length ;i++){
+        var property =  properties[i]
+        var propertyType = property[0]
+        var propertyKey = property[1]
+        if (propertyType === "ObjectProperty") {
+            __ohdAssign(obj, property[1], property[2])
+        } else if (propertyType === "ObjectMethod") {
+            var propertyKind = property[2]
+            var fn = property[3]
+            if (!methodProperties[propertyKey]){
+                methodProperties[propertyKey] = {
+                    enumerable: true,
+                    configurable: true
+                }
+            }
+            methodProperties[propertyKey][propertyKind] = fn
+
+        }
+    }
+    Object.defineProperties(obj, methodProperties)
+    return obj
+}
--- ohd.spec.js
+++ ohd.spec.js
@@ -1,5 +1,42 @@
-describe("sth", function(){
-    it("doesntwork", function(){
-        expect("cake").toBe("sss")
+describe("Object History Debugger", function(){
+    var objWithHistory = __ohdMakeObject([['ObjectProperty', 'age', 77]])
+    var obj = {age: 77}
+
+    it("Has an age__history__ property", function(){
+        expect(objWithHistory.age__history__).not.toBe(undefined)
+    })
+
+    it("Behaves like a normal object in forEach", function(){
+        var keys = [];
+        var keysInWithHistory = [];
+        for (key in obj) {
+            keys.push(key)
+        }
+        for (key in objWithHistory) {
+            keysInWithHistory.push(key)
+        }
+
+        expect(keys).toEqual(keysInWithHistory)
+    })
+
+    it("Behaves like a normal object when getting Object.keys", function(){
+        var keys = Object.keys(obj);
+        var keysInWithHistory = Object.keys(objWithHistory);
+
+        expect(keys).toEqual(keysInWithHistory)
+    })
+
+    it("Behaves like a normal object when getting Object.getOwnPropertyNames", function(){
+        var names = Object.getOwnPropertyNames(obj);
+        var namesInWithHistory = Object.getOwnPropertyNames(objWithHistory);
+
+        expect(names).toEqual(namesInWithHistory)
+    })
+
+    it("Behaves like a normal object when getting Object.getOwnPropertyDescriptors", function(){
+        var descriptors = Object.getOwnPropertyDescriptors(obj);
+        var descriptorsInWithHistory = Object.getOwnPropertyDescriptors(objWithHistory);
+
+        expect(Object.keys(descriptors)).toEqual(Object.keys(descriptorsInWithHistory))
     })
 })
