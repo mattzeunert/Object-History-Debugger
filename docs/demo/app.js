409c672e72e4f631f99ac62b08b7f335847322b4
Matt Zeunert <matt@mostlystatic.com>
2017-01-16 10:33:49 +0700

    Make source maps work for demo.

--- docs/demo/app.js
+++ docs/demo/app.js
@@ -40,17 +40,51 @@
                 </tr>
                 <tr>
                     <td>History Length</td>
-                    <td>"${message.text__history__.fullHistory.length}"</td>
+                    <td>${message.text__history__.fullHistory.length}</td>
                     <td>${message.bold__history__.fullHistory.length}</td>
+                </tr>
+                <tr>
+                    <td>Last change location</td>
+                    <td>${fileNameOnly(message.text__history__.fullHistory[0].stack[0])}</td>
+                    <td>${fileNameOnly(message.bold__history__.fullHistory[0].stack[0])}</td>
+                </tr>
+                <tr>
+                    <td>Last change location<br> (asynchronous, sourcemapped)</td>
+                    <td id="last-change-text"></td>
+                    <td id="last-change-bold"></td>
                 </tr>
             </table>
         `
 
         messageObjectInfo.innerHTML = html;
+
+        updateLastLocationSourcemapped(message.text__history__, "#last-change-text")
+        updateLastLocationSourcemapped(message.bold__history__, "#last-change-bold")
+
+        function updateLastLocationSourcemapped(history, selector){
+            document.querySelector(selector).textContent = ""
+            codePreprocessor.resolveFrame(history.fullHistory[0].stack[0], function(err, frame){
+                // artificial delay to show it's asynchronous
+                setTimeout(function(){
+                    document.querySelector(selector).textContent = fileNameOnly(frame.fileName) + ":" + frame.lineNumber + ":" + frame.columnNumber
+                }, 200)
+            })
+        }
+
+        function fileNameOnly(fileName){
+            var isDist = fileName.indexOf("/dist/") !== -1;
+            var parts = fileName.split("/")
+
+            var ret = isDist ? "dist/" : ""
+            ret += parts[parts.length - 1]
+
+            return ret
+        }
     }
 
     updateUI()
 
     console.log("This is the global message object:", message)
+
 
 })()
