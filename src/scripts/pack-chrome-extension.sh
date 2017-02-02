50b6520a190beb54e46b4c59cb1622afabd443c3
Matt Zeunert <matt@mostlystatic.com>
2017-02-02 17:09:23 +0700

    Update Chrome extension.

--- src/scripts/pack-chrome-extension.sh
+++ src/scripts/pack-chrome-extension.sh
@@ -1,10 +1,10 @@
 mkdir chrome-extension-dist
-cp babel-plugin.js chrome-extension-dist/babel-plugin.js
-cp ChromeCodeInstrumenter.js chrome-extension-dist/ChromeCodeInstrumenter.js
-cp background.html chrome-extension-dist/background.html
-cp background.js chrome-extension-dist/background.js
-cp ohd.js chrome-extension-dist/ohd.js
-cp manifest.json chrome-extension-dist/manifest.json
-cp icon.png chrome-extension-dist/icon.png
+cp src/babel-plugin.js chrome-extension-dist/babel-plugin.js
+cp src/ChromeCodeInstrumenter.js chrome-extension-dist/ChromeCodeInstrumenter.js
+cp src/background.html chrome-extension-dist/background.html
+cp src/background.js chrome-extension-dist/background.js
+cp src/ohd.js chrome-extension-dist/ohd.js
+cp src/manifest.json chrome-extension-dist/manifest.json
+cp src/icon.png chrome-extension-dist/icon.png
 zip -r ohd-extension.zip ./chrome-extension-dist/
 rm -r chrome-extension-dist
